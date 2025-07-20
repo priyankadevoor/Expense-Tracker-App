4835fb8ad5575ef9c97f08d381b6cca0dd9e97f3
priyankadevoor <priyadevoor@gmail.com>
2025-07-20 11:33:02 -0400

    Created User DTO model

---
+++ app/src/main/java/org/example/model/UserInfoDto.java
@@ -0,0 +1,19 @@
+package org.example.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming (PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class UserInfoDto extends org.example.entities.UserInfo
+{
+
+    private String firstName; // first_name
+
+    private String lastName; //last_name
+
+    private Long phoneNumber;
+
+    private String email; // email
+
+
+}
